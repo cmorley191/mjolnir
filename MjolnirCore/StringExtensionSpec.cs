c938a709d1665af99b45130d78ba1dcccccece60
JamesMReilly <jimreilly28@gmail.com>
2018-04-25 10:34:43 -0500

    finish testing string extension

--- MjolnirCore/StringExtensionSpec.cs
+++ MjolnirCore/StringExtensionSpec.cs
@@ -31,6 +31,25 @@ namespace MjolnirCore {
 			Assert.AreNotEqual("not returned by a ToString()", foo.ToObjectString());
 		}
 
+		[Test]
+		public void ConvertAKeyValuePairToAnObjectString() {
+			var arbitrarypair = new KeyValuePair<int, string>(30, "i print this when tested!");
+			Assert.AreEqual("{ 30, i print this when tested! }", arbitrarypair.ToObjectString());
+		}
+
+		[Test]
+		public void ConvertADictionaryToADictString() {
+			var arbitrarypair1 = new KeyValuePair<int, string>(30, "i print this when tested!");
+			var arbitrarypair2 = new KeyValuePair<int, string>(31, "why have you done this?");
+
+			var dict = new Dictionary<int, string>();
+			dict.Add(arbitrarypair1.Key, arbitrarypair1.Value);
+			dict.Add(arbitrarypair2.Key, arbitrarypair2.Value);
+
+			var expectedString = "{\n\t{ 30, i print this when tested! },\n\t{ 31, why have you done this? }\n}";
+			Assert.AreEqual(expectedString, dict.ToDictString());
+		}
+
 		private class NonTObj {
 			private readonly string testField;
 			public NonTObj() {
