3ae9a5da196a9fcc15135e7f3769a5da9e343220
JamesMReilly <jimreilly28@gmail.com>
2018-04-25 10:47:57 -0500

    test optionextension in c#

---
+++ MjolnirCore/OptionExtensionSpec.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using MjolnirCore.Extensions;
+using Microsoft.FSharp.Core;
+
+
+namespace MjolnirCore {
+
+	[TestFixture]
+	public class OptionExtensionSpec {
+		
+		private readonly FSharpOption<int> nullOption = null;
+		private readonly FSharpOption<int> nonNullOption = new FSharpOption<int>(1);
+
+		[Test]
+		public void IsNoneOnNullObjectIsTrue() {
+			Assert.IsTrue(nullOption.IsNone());
+		}
+
+		[Test]
+		public void IsNoneOnNonNullObjectIsFalse() {
+			Assert.IsFalse(nonNullOption.IsNone());
+		}
+
+		[Test]
+		public void IsSomeOnNonNullObjectIsTrue() {
+			Assert.IsTrue(nonNullOption.IsSome());
+		}
+
+		[Test]
+		public void IsSomeOnNullObjectIsFalse() {
+			Assert.IsFalse(nullOption.IsSome());
+		}
+
+		[Test]
+		public void TryGetValueOnNonNullObjectCanGetValue() {
+			int foo;
+			Assert.IsTrue(nonNullOption.TryGetValue(out foo));
+			Assert.AreEqual(1, foo);
+		}
+
+		[Test]
+		public void TryGetValueOnNullObjectReturnsFalse() {
+			int foo;
+			Assert.IsFalse(nullOption.TryGetValue(out foo));
+		}
+
+	}
+}
